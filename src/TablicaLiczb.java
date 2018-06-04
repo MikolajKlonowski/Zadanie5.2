6aebd94fccb8206469ec9d5988aa832bb495983e
Mikołaj Klonowski <nick-93@o2.pl>
2018-06-04 16:53:23 +0200

    Zadanie 9

---
+++ src/TablicaLiczb.java
@@ -0,0 +1,19 @@
+public class TablicaLiczb {
+    public static void main(String[] args) {
+        int [] tablica = new int [5];
+        tablica[0] = 1;
+        tablica[1] = 2;
+        tablica[2] = 3;
+        tablica[3] = 4;
+        tablica[4] = 5;
+
+        System.out.println(tablica[0]+tablica[2]+tablica[4]);
+
+
+
+
+        }
+
+
+
+    }
